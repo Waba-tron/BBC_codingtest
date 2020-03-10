b0c34fbb3faab380e42ca250450f655d841c0d5c
Wahab Rehman <wahab_rehman1256@outlook.com>
2020-03-10 23:30:26 +0000

    first commit

---
+++ rankScript.js
@@ -0,0 +1,59 @@
+
+
+function makePosts(e){
+
+    //User inputs
+    let highestRank = document.getElementById('first').value;
+    let secondRank = document.getElementById('second').value;
+    let thridRank = document.getElementById('thrid').value;
+
+    //Ranking outputs
+    let rank1 = document.getElementById('rank1');
+    let rank2 = document.getElementById('rank2');
+    let rank3 = document.getElementById('rank3');
+
+    e.preventDefault();
+
+    //Checks to see if user has listed the same article for a specific
+    if(highestRank == secondRank || highestRank == thridRank || secondRank == thridRank){
+        rank1.textContent = `Can not have the same articles for ranking`;
+        rank2.textContent = ``;
+        rank3.textContent = ``;
+    }
+    else{
+
+        //makes post request to the url or api even
+        fetch('https://jsonplaceholder.typicode.com/posts',{
+
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json, text/plain, */*',
+                'Content-type': 'application/json'
+            },
+            mode: 'cors',
+
+            //uses the api api to submit our json object
+            body:JSON.stringify({highestRank: highestRank, middleRank: secondRank, lowestRank: thridRank})
+
+        })
+
+        .then(result => {
+
+            
+            return result.json();
+        })
+
+        .then(data => {
+
+            //out puts users ranking of articles
+            rank1.textContent = `You have chosen article ${data.highestRank} have the highest ranking`;
+            rank2.textContent = `You have chosen article ${data.middleRank} have the middle ranking`;
+            rank3.textContent = `You have chosen article ${data.lowestRank} have the lowest ranking`;
+
+        })
+
+    }
+}
+
+//function that intialises post request
+document.getElementById('postRanking').addEventListener('submit', makePosts);
---
+++ script.js
@@ -0,0 +1,106 @@
+//Buttons
+let btnArticle1 = document.getElementById("article1Btn1");
+let btnArticle2 = document.getElementById("article1Btn2");
+let btnArticle3 = document.getElementById("article1Btn3");
+let btnrandomArticle = document.getElementById("randomarticle1Btn");
+
+//Json Data 
+let article2Data = "https://raw.githubusercontent.com/bbc/news-coding-test-dataset/master/data/article-2.json";
+let article3Data = "https://raw.githubusercontent.com/bbc/news-coding-test-dataset/master/data/article-3.json";
+let article5Data = "https://raw.githubusercontent.com/bbc/news-coding-test-dataset/master/data/article-5.json";
+
+//function to generate article
+function generateArticle(url) {
+
+    fetch(url)
+
+    .then(result => {
+
+        return result.json();
+    })
+
+    .then(data => {
+
+        let articleTitle = (data.title); //Generate the title
+        let articleHeading = (data.body[0].model.text); //Generate Heading
+        let randomNumber = 0;
+        let articleContent = ""; //Will hold all the content
+
+        for (let i = 0; i < data.body.length; i++) {
+
+            if (data.body[i].type == "paragraph") {
+
+                //if type is a paragraph at it to the content of the page
+                articleContent += `<p>${data.body[i].model.text}</p>`;
+
+            }
+
+            else if (data.body[i].type == "image") {
+
+                //generate a random number and put it next to the image url to stop images from being the same
+                randomNumber = Math.floor(Math.random() * 100) + 1;
+
+                //if type is a image at it to the content of the page
+                articleContent += `<img src="${data.body[i].model.url}${randomNumber}" alt="${data.body[i].model.altText}" width="${data.body[i].model.width}" height="${data.body[i].model.height}"><br><br>`;
+
+            }
+
+        }
+
+        //Adding all the data in to the DOM
+        document.getElementsByTagName("title")[0].innerHTML = `${articleTitle}`;
+        document.getElementsByTagName("section")[0].innerHTML = `<h1>${articleHeading}</h1>
+     
+        ${articleContent}
+        `;
+
+    });
+}
+
+//Display first article
+btnArticle1.addEventListener("click", function(){
+
+    url = article2Data;
+    generateArticle(url);
+
+});
+
+//Display second article
+btnArticle2.addEventListener("click", function(){
+
+    url = article3Data;
+    generateArticle(url);
+
+});
+
+//Display thrid article
+btnArticle3.addEventListener("click", function(){
+    
+    url = article5Data;
+    generateArticle(url);
+
+});
+
+//function display random article
+btnrandomArticle.addEventListener("click", function(){
+
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+
+    if(randomNumber == 1){
+
+        url = article2Data;
+    }
+    else if(randomNumber == 2){
+
+        url = article3Data;
+    }
+    else{
+
+        url = article5Data;
+    }
+
+    generateArticle(url);
+
+});
+
+
